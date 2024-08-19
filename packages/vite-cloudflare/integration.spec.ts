16b7f363d5b0b31aa37e567a4c66d6adc13d0d28
Jacob Ebey <jacob.ebey@live.com>
2024-08-18 19:27:47 -0700

    revert integration test setup

--- packages/vite-cloudflare/integration.spec.ts
+++ packages/vite-cloudflare/integration.spec.ts
@@ -1,32 +1,45 @@
 import * as assert from "node:assert/strict";
 import { after, describe, test, before } from "node:test";
+import * as net from "node:net";
 
-import * as vite from "vite";
-
-import type { CloudflareDevEnvironment } from "./src/index.js";
+import type { ResultPromise } from "execa";
+import { execa } from "execa";
 
 describe("integration", () => {
-  let server: vite.ViteDevServer;
-  let workera: CloudflareDevEnvironment;
-
+  let url: string;
+  let vitePromise: ResultPromise<{
+    all: true;
+    cwd: string;
+  }>;
   before(async () => {
-    server = await vite.createServer({
-      root: "fixture",
-    });
-    workera = server.environments.workera as CloudflareDevEnvironment;
-  });
+    const port = await getPort();
+    vitePromise = execa({
+      all: true,
+      cwd: "fixture",
+    })`pnpm dev --port ${port}`;
 
+    await waitForPort(port);
+    url = `http://localhost:${port}`;
+  });
   after(async () => {
-    await server.close();
+    if (!vitePromise.kill()) {
+      throw new Error("Failed to kill Vite");
+    }
+    const exited = await vitePromise;
+    if (!vitePromise.killed) {
+      throw new Error("Vite did not exit");
+    }
+    assert.strictEqual(
+      exited.exitCode,
+      0,
+      `Vite exited with code ${exited.exitCode}`
+    );
   });
 
   test("should resolve proper conditions and allow local DurableObject, and Service bindings", async () => {
-    const response = await workera.dispatchFetch(
-      new Request("http://test.dev/")
-    );
-    assert.strictEqual(response.status, 200);
-
+    const response = await fetch(url);
     const json = await response.json();
+
     assert.deepStrictEqual(json, {
       environmentSpecific: "a",
       workerB: {
@@ -39,3 +52,41 @@ describe("integration", () => {
     });
   });
 });
+
+async function waitForPort(port: number) {
+  while (true) {
+    try {
+      await new Promise<void>((resolve, reject) => {
+        const socket = net.connect(port);
+        socket.on("connect", () => {
+          socket.end();
+          resolve();
+        });
+        socket.on("error", (error) => {
+          reject(error);
+        });
+      });
+      return;
+    } catch {
+      await new Promise<void>((resolve) => {
+        setTimeout(resolve, 100);
+      });
+    }
+  }
+}
+
+async function getPort() {
+  const server = net.createServer();
+  await new Promise<void>((resolve) => {
+    server.listen(0, () => {
+      resolve();
+    });
+  });
+  const address = server.address();
+  if (typeof address === "string" || !address) {
+    throw new Error("Unexpected address");
+  }
+  const port = address.port;
+  server.close();
+  return port;
+}
