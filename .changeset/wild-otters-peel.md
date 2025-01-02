34c29eaea294f89cc87803c57cf189b3fe7acd21
Jacob Ebey <jacob.ebey@live.com>
2025-01-02 14:37:52 -0800

    feat!: simplify react server plugin

---
+++ .changeset/wild-otters-peel.md
@@ -0,0 +1,5 @@
+---
+"@jacob-ebey/vite-react-server-dom": patch
+---
+
+simplify react server plugin responsibilities
--- packages/vite-react-server-dom/tsup.config.ts
+++ packages/vite-react-server-dom/tsup.config.ts
@@ -2,13 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig([
   {
-    entry: [
-      "src/index.ts",
-      "src/references-server.ts",
-      "src/worker-development.ts",
-      "src/worker-production.ts",
-      "src/worker-shared.ts",
-    ],
+    entry: ["src/index.ts"],
     format: "esm",
     platform: "node",
     dts: true,
